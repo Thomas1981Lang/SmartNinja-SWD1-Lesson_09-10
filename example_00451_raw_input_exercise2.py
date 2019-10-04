7e50b934410c5e9f91b7d06ac13067dc0c94460a
Thomas Lang <34832500+Thomas1981Lang@users.noreply.github.com>
2019-10-04 20:55:25 +0200

    raw input exercise

--- example_00451_raw_input_exercise2.py
+++ example_00451_raw_input_exercise2.py
@@ -1,3 +1,9 @@
 # ask for two string to be inputted, and save it in a variables
 # check if both variables are equal
+var_one = input('Please Input Text One: ')
+var_two = input('Please Input Text Two: ')
 
+if var_one == var_two:
+    print('equal')
+else:
+    print('not equal')
